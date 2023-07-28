8721cab7c76cf0343c60b0043d71075f1c2b5908
ShemenV <103890522+ShemenV@users.noreply.github.com>
2023-07-29 01:59:49 +0500

    Сделал вкладки

---
+++ app/src/main/java/com/novikov/blubb/MainFragment.kt
@@ -0,0 +1,65 @@
+package com.novikov.blubb
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.novikov.blubb.adapters.MainFragmentTabsAdapter
+import com.novikov.blubb.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    private val adapterViewPager by lazy {
+        MainFragmentTabsAdapter(
+            childFragmentManager,
+            lifecycle
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Chats"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Servers"))
+
+        binding.viewPager.adapter = adapterViewPager
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    binding.viewPager.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+        })
+
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tabLayout.selectTab(binding.tabLayout.getTabAt(position))
+            }
+        })
+    }
+
+
+}
