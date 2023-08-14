c4026d7e370702e9e37d0176c32144f2a6675e07
IliaNovikov <ilia.pp.novikov@gmail.com>
2023-08-14 15:31:09 +0500

    проблемный hilt

--- app/src/main/java/com/novikov/blubb/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/novikov/blubb/presentation/activities/MainActivity.kt
@@ -3,7 +3,9 @@ package com.novikov.blubb.presentation.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.novikov.blubb.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
