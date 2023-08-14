e41c091c8c3b7a8e667b9fe535856d045b6a2788
IliaNovikov <ilia.pp.novikov@gmail.com>
2023-08-14 14:36:13 +0500

    подключил hilt

--- app/src/main/java/com/novikov/blubb/presentation/fragments/AuthorizationFragment.kt
+++ app/src/main/java/com/novikov/blubb/presentation/fragments/AuthorizationFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContentProviderCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
@@ -27,24 +28,28 @@ class AuthorizationFragment : Fragment() {
         binding = FragmentAuthorizationBinding.inflate(layoutInflater)
         auth = Firebase.auth
 
-        val login = binding.editTextNickname.text.toString()
-        val password = binding.editTextPassword.text.toString()
+        binding.buttonLogin.setOnClickListener {view ->
 
-        auth.signInWithEmailAndPassword(login, password).addOnCompleteListener {
+            val login = binding.editTextNickname.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(login, password).addOnCompleteListener {
             if (it.isSuccessful) {
-                Toast.makeText(
-                    requireContext(),
+                Snackbar.make(
+                    view,
                   "Вы вошли в аккаунт",
-                    Toast.LENGTH_SHORT
+                    Snackbar.LENGTH_SHORT
                 ).show()
             } else {
-                Toast.makeText(
-                  requireContext(),
+                Snackbar.make(
+                  view,
                     resources.getText(R.string.authorization_error),
-                    Toast.LENGTH_SHORT
+                    Snackbar.LENGTH_SHORT
                 ).show()
             }
         }
+        }
+
         return binding.root
     }
 
