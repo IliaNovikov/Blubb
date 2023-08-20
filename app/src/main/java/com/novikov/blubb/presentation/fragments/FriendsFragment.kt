4854451f1bf85f2316283ea7edd77470ec9bc0b1
IliaNovikov <ilia.pp.novikov@gmail.com>
2023-08-20 17:27:30 +0500

    верстка поиска пользователей + id

--- app/src/main/java/com/novikov/blubb/presentation/fragments/FriendsFragment.kt
+++ app/src/main/java/com/novikov/blubb/presentation/fragments/FriendsFragment.kt
@@ -5,18 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.novikov.blubb.R
+import com.novikov.blubb.databinding.FragmentFriendsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FriendsFragment : Fragment() {
 
+    private lateinit var binding: FragmentFriendsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false)
+
+        binding = FragmentFriendsBinding.inflate(layoutInflater)
+
+        binding.buttonAddFriends.setOnClickListener {
+            requireActivity()
+                .findNavController(R.id.nav_host_fragment)
+                .navigate(R.id.action_friendsFragment_to_searchUsersFragment)
+        }
+
+        return binding.root
     }
 
 }
